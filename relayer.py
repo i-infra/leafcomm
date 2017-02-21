1736eceaafd01c3c950c0b769f440e49b4059bef
Ian Daniher <itdaniher@gmail.com>
2017-02-21 23:07:16 +0100

    improvements to relayer - print errors shortest to longest, and print base64'd error timestamp to console

--- relayer.py
+++ relayer.py
@@ -2,26 +2,30 @@ import asyncio
 import beepshrink
 import phase1
 import cbor
-
+import base64
 import sys
 
 async def main():
-    reader, writer = await asyncio.open_connection(sys.argv[1], int(sys.argv[2]))
+    try:
+        reader, writer = await asyncio.open_connection(sys.argv[1], int(sys.argv[2]))
+    except:
+        writer = None
     conn = await phase1.get_connection()
-    failed = await conn.smembers('nontrivial_timestamps')
+    failed = await conn.smembers_asset('nontrivial_timestamps')
     byte_count = 0
-    infos = await conn.mget(sorted([await x for x in failed]))
     def forwarder(info):
-        ba = beepshrink.decompress(info['size'], info['dtype'], info['data'])*255
-        writer.write(ba.tobytes())
-        ab = ba*0.0
-        writer.write(ab.tobytes())
-        return len(ab)*2
-    for info in infos:
-        info = await info
-        if (info is not None) and (info is not {}):
-            byte_count += forwarder(info)
-            await writer.drain()
+        if writer is not None:
+            ba = beepshrink.decompress(**info)*255
+            writer.write(ba.tobytes())
+            ab = ba*0.0
+            writer.write(ab.tobytes())
+            return len(ab)*2
+        return 0
+    infos = await conn.mget_aslist(sorted(failed))
+    for info in sorted([x for x in zip(sorted(failed),infos) if x[1] is not None and 'size' in x[1].keys()], key=lambda x: x[1]['size']):
+        print(base64.b64encode(cbor.dumps(info[0])), info[1]['size'])
+        byte_count += forwarder(info[1])
+        await writer.drain()
     print('bytes sent', byte_count)
 
 if __name__ == "__main__":
