af7a6bbe55319acce78e8e98d0b67b42d6962d10
Ian Daniher <itdaniher@gmail.com>
2017-02-21 02:27:35 +0100

    write decompressed errors, with backpressure, to a specified tcp tuple

--- relayer.py
+++ relayer.py
@@ -9,12 +9,20 @@ async def main():
     reader, writer = await asyncio.open_connection(sys.argv[1], int(sys.argv[2]))
     conn = await phase1.get_connection()
     failed = await conn.smembers('nontrivial_timestamps')
-    for ts in sorted([await x for x in failed]):
-        print(ts)
-        info = await conn.get(ts) 
+    byte_count = 0
+    infos = await conn.mget(sorted([await x for x in failed]))
+    def forwarder(info):
         ba = beepshrink.decompress(info['size'], info['dtype'], info['data'])*255
-        if ba is not None:
-            writer.write(ba.tobytes())
+        writer.write(ba.tobytes())
+        ab = ba*0.0
+        writer.write(ab.tobytes())
+        return len(ab)*2
+    for info in infos:
+        info = await info
+        if (info is not None) and (info is not {}):
+            byte_count += forwarder(info)
+            await writer.drain()
+    print('bytes sent', byte_count)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
