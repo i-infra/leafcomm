3237b9de6bf2cf31abcd7dfab63cb52f5ca22d39
Ian Daniher <itdaniher@gmail.com>
2017-03-19 06:25:03 +0000

    whitespace

--- scripts/stft.py
+++ scripts/stft.py
@@ -10,13 +10,13 @@ def stftw(input_vector, sampling_rate, frame_duration, increment_duration):
     output_array = pyfftw.empty_aligned(frame_length, dtype='complex64')
     fft_runner = pyfftw.FFTW(input_array, output_array)
     window = np.hanning(frame_length).astype('complex64')
-    X = np.zeros((len(starting_points)+1, frame_length), dtype='complex64') 
+    X = np.zeros((len(starting_points)+1, frame_length), dtype='complex64')
     acc = 0
-    for i in starting_points: 
+    for i in starting_points:
         np.copyto(input_array, window*input_vector[i:i+frame_length], casting='no')
         fft_runner()
         np.copyto(X[acc], np.array(output_array))
-        acc += 1 
+        acc += 1
     return X.reshape((acc+1, frame_length))
 
 def stft(x, fs, framesz, hop):
