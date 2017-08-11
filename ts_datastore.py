5c5855f73cdb96fe64d49cd9bc3cccf65178d8eb
Ian Daniher <itdaniher@gmail.com>
2017-08-11 03:30:59 +0000

    datastore v1, includes meta column and sample tags

--- ts_datastore.py
+++ ts_datastore.py
@@ -3,7 +3,14 @@ import json
 import random
 import time
 
-unit_list = ['none', 'degc', 'rh', 'pa', 'watt', 'ph', 'ms_per_cm', 'umol_per_m2_per_sec', 'ppm_co2', 'ppm_o', 'ppm_n', 'ppm_p', 'ppm_k', 'ppm_na', 'ppm_ca', 'ppm_mg', 'ppm_cl']
+# TODO: use pint to refine this?
+# used in meta tag to modify the set of compound units
+si_units = ['m', 'kg', 's', 'A', 'K', 'mol', 'cd']
+si_human = ['meter', 'kilogram', 'second', 'ampere', 'Degree Kelvin', 'moles', 'candela']
+# compound units
+unit_list = ['none', 'degc', 'rh', 'pa', 'watt', 'ppm', 'ph', 'ms_per_cm', 'umol_per_m2_s']
+unit_human = ['None', 'Degrees Celsius', 'Relative Humidity', 'Pascals', 'Watts', 'Parts Per Million', 'pH', 'Conductivity', 'Photosynthetically Active Radiation (PAR)']
+meta_tags = ['human:', 'per:', 'times:', 'ChEBI:']
 # Specs for Lettuce
 # 18-25 degC
 # RH 50-70 %
@@ -24,7 +31,6 @@ unit_list = ['none', 'degc', 'rh', 'pa', 'watt', 'ph', 'ms_per_cm', 'umol_per_m2
 # http://ceac.arizona.edu/
 # http://www.greenhouse.cornell.edu/crops/factsheets/hydroponic-recipes.pdf
 # http://www.cornellcea.com/attachments/Cornell%20CEA%20Lettuce%20Handbook%20.pdf 
-unit_human = ['None', 'Degrees Celsius', 'Relative Humidity', 'Pascals', 'Watts', 'pH', 'Conductivity', 'Photosynthetically Active Radiation (PAR)' 'PPM Carbon Dioxide', 'PPM Dissolved Oxygen', 'PPM Nitrate', 'PPM Phosphorus', 'PPM Potassium', 'PPM Sodium', 'PPM Calcium', 'PPM Magnesium', 'PPM Chloride']
 
 # kludge to pull enum into local namespace
 for i, unit in enumerate(unit_list):
@@ -33,9 +39,9 @@ del i, unit
 
 class TimeSeriesDatastore(object):
 
-    def __init__(self, db = 'sproutwave_v0.db'):
-        self.conn = sql.connect('sproutwave_v0.db')
-        init_readings = 'CREATE TABLE IF NOT EXISTS readings (Timestamp REAL, Sensor INT, Units INT, Value REAL)'
+    def __init__(self, db_name = 'sproutwave_v1.db'):
+        self.conn = sql.connect(db_name)
+        init_readings = 'CREATE TABLE IF NOT EXISTS readings (Timestamp REAL, Sensor INT, Units INT, Value REAL, Meta TEXT)'
         self.conn.execute(init_readings)
         create_index = 'CREATE INDEX IF NOT EXISTS time ON readings (Timestamp ASC)'
         self.conn.execute(create_index)
@@ -48,7 +54,7 @@ class TimeSeriesDatastore(object):
         selector = 'SELECT * FROM readings WHERE Timestamp BETWEEN %s AND %s' % (
             str(start), str(stop))
         samples = self.cursor.execute(selector)
-        labels = 'timestamp sensor_uid units value'.split()
+        labels = 'timestamp sensor_uid units value meta'.split()
         values = dict(zip(labels, zip(*samples)))
         values['units'] = [unit_list[unit] for unit in values['units']]
         return values
@@ -59,13 +65,13 @@ class TimeSeriesDatastore(object):
         selector = 'SELECT * FROM readings WHERE Timestamp BETWEEN %s AND %s' % (
             str(start), str(stop))
         samples = self.cursor.execute(selector)
-        labels = 'timestamp sensor_uid units value'.split()
+        labels = 'timestamp sensor_uid units value meta'.split()
         samples = [dict(zip(labels, sample)) for sample in samples]
         for sample in samples:
            sample['units'] = unit_list[sample['units']]
         return samples
 
-    def add_measurement(self, timestamp, sensor_uid, units, value, raw = False):
+    def add_measurement(self, timestamp, sensor_uid, units, value, raw = False, meta = None):
         if raw == True:
             unit_tag = units
         else:
@@ -79,8 +85,8 @@ class TimeSeriesDatastore(object):
             value = float(value)
         if type(sensor_uid) != int:
             sensor_id = int(sensor_id)
-        inserter = "INSERT INTO readings VALUES(?, ?, ?, ?)"
-        data = (timestamp, sensor_uid, unit_tag, value)
+        inserter = "INSERT INTO readings VALUES(?, ?, ?, ?, ?)"
+        data = (timestamp, sensor_uid, unit_tag, value, meta)
         self.cursor.executemany(inserter, [data])
         return self.conn.commit()
 
