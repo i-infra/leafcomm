895026a86e88516ee302387366da82ed8601f6ff
Ian Daniher <itdaniher@gmail.com>
2017-12-15 23:21:51 +0000

    dumper.py defaults to all unexpired in nontrivial_timestamps

--- scripts/dumper.py
+++ scripts/dumper.py
@@ -7,18 +7,20 @@ import node_core
 import cbor
 import base64
 
-
-# accepts one argument, the ascii base64 representation of the cbor-encoded f64 timestamp
-# this is awkward, but unambiguous and space-efficient. you can at least type it, don't complain.
-
 async def main():
     conn = await asyncio_redis.Connection.create('localhost', 6379, encoder=node_core.CborEncoder())
-    for arg in sys.argv[1::]:
-        key = cbor.loads(base64.b64decode(arg))
+    keys = sys.argv[1::]
+    if len(keys) == 0:
+        keys = [await x for x in await conn.smembers('nontrivial_timestamps')]
+    else:
+        keys = [cbor.loads(base64.b64decode(arg)) for arg in keys]
+    print(keys)
+    for key in keys:
         info = await conn.get(key)
-        f = open(str(key)+'.beep', 'wb')
-        f.write(cbor.dumps(info))
-        f.flush()
+        if (info != None):
+            f = open(str(key)+'.beep', 'wb')
+            f.write(cbor.dumps(info))
+            f.flush()
     conn.close()
 
 if __name__ == "__main__":
