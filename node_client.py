bc663572ea55503b176d5448795c52db7c166e14
Ian Daniher <itdaniher@gmail.com>
2018-01-18 20:23:30 +0000

    all to 8019

--- node_client.py
+++ node_client.py
@@ -2,7 +2,7 @@ from node_proxy import *
 
 async def test_ws():
     with aiohttp.client.ClientSession() as client:
-        test_ws = await client.ws_connect('https://data.sproutwave.com:8444/ws')
+        test_ws = await client.ws_connect('https://data.sproutwave.com:8019/ws')
         human_name, uid = get_hardware_uid()
         test_ws.send_bytes(uid)
         while True:
