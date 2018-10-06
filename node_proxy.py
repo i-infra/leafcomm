95ac272d73323892043b0345c7baebd8ae9673e7
Ian Daniher <itdaniher@gmail.com>
2018-10-06 05:58:19 +0000

    rework node_proxy fixing websocket connections blocking the redis connection

--- node_proxy.py
+++ node_proxy.py
@@ -31,7 +31,7 @@ class ProxyDatagramProtocol(asyncio.DatagramProtocol):
         self.loop.create_task(pseudopub(self.connection, ['udp_inbound'], None, data))
 
 async def register_node(request):
-    connection = request.app['connection']
+    connection = request.app['redis']
     posted_bytes = await request.read()
     pubkey_bytes, uid = unbox_semisealed(posted_bytes)
     session_id = nacl.utils.random(nacl.public.Box.NONCE_SIZE)
@@ -58,37 +58,22 @@ async def redistribute(loop=None):
                 await pseudopub(redis_connection, [uid], None, update_msg)
 
 async def init_ws(request):
+    redis_connection = await init_redis("proxy")
     logging.debug('ws request started')
-    redis_connection = request.app['connection']
     ws = web.WebSocketResponse()
     await ws.prepare(request)
-    msg = await ws.receive()
-    print(msg)
-    uid = msg.data
+    access_token = await ws.receive()
+    print(access_token)
+    uid = access_token.data
     json_bytes = await redis_connection.hget('most_recent', uid)
     if json_bytes is not None:
         await ws.send_str(json_bytes.decode())
     while True:
-        ws_closed = False
-        ts, update_msg = await pseudosub1(redis_connection, uid, do_tick=False)
+        ts, update_msg = await pseudosub1(redis_connection, uid, timeout=1, do_tick=False)
         if update_msg:
-            try:
-                json_bytes = json.dumps(update_msg)
-            except:
-                json_bytes = None
-        else:
-            json_bytes = None
-        try:
-            msg = await asyncio.wait_for(ws.receive(), 1)
-            print(msg)
-            if msg.type in [aiohttp.WSMsgType.ERROR, aiohttp.WSMsgType.CLOSE, aiohttp.WSMsgType.CLOSED, aiohttp.WSMsgType.CLOSING]:
-                break
-        except asyncio.TimeoutError:
-            print(ws, 'timed out')
-            pass
-        if json_bytes:
-            await redis_connection.hset('most_recent', uid, json_bytes)
+            json_bytes = json.dumps(update_msg)
             await ws.send_str(json_bytes)
+            await redis_connection.hset('most_recent', uid, json_bytes)
     return ws
 
 def unbox_semisealed(byte_blob, secret_key = relay_secret_key):
@@ -99,17 +84,22 @@ def unbox_semisealed(byte_blob, secret_key = relay_secret_key):
     return raw_pubkey, contents
 
 async def signup(request):
-    connection = request.app['connection']
+    connection = request.app['redis']
     posted_bytes = await request.read()
     msg = cbor.loads(posted_bytes)
     return web.json_response(msg)
 
 def create_app(loop):
+    async def start_background_tasks(app):
+        app['redis'] = await init_redis("proxy")
+        protocol = ProxyDatagramProtocol(loop, await init_redis("proxy"))
+        app['udp_task'] = loop.create_task(loop.create_datagram_endpoint(lambda: protocol, local_addr=('0.0.0.0', _constants.upstream_port)))
+        app['redistribute_task'] = loop.create_task(redistribute())
     app = web.Application()
-    app['redis_connection'] = None
     app.router.add_get('/ws', init_ws)
     app.router.add_post('/register', register_node)
     app.router.add_post('/signup', signup)
+    app.on_startup.append(start_background_tasks)
     return app
 
 if __name__ == "__main__":
@@ -117,11 +107,6 @@ if __name__ == "__main__":
     handlebars.start_redis_server(redis_socket_path=data_dir+'proxysproutwave.sock')
     loop = asyncio.get_event_loop()
     app = create_app(loop)
-    handlebars.multi_spawner(redistribute)
-    app['connection'] = loop.run_until_complete(asyncio.ensure_future(init_redis("proxy")))
-    protocol = ProxyDatagramProtocol(loop, loop.run_until_complete(asyncio.ensure_future(init_redis("proxy"))))
-    print(app['connection'])
-    loop.create_task(loop.create_datagram_endpoint(lambda: protocol, local_addr=('0.0.0.0', _constants.upstream_port)))
     if _constants.upstream_protocol == 'https':
         context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
         context.load_cert_chain(certfile=local_dir+"/resources/fullchain.pem", keyfile=local_dir+"/resources/privkey.pem")
