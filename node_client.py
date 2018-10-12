390ad82c2c41d4b2d19f9f6fcfbb4c2ded8b714c
Ian Daniher <itdaniher@gmail.com>
2018-10-12 22:07:19 +0000

    lightly exercise signup endpoint with test_signup

--- node_client.py
+++ node_client.py
@@ -1,5 +1,17 @@
 from node_proxy import *
+from node_core import *
 import _constants
+
+async def test_signup():
+    redis_connection = await init_redis()
+    pubkey_bytes, session_box = create_ephemeral_box_from_pubkey_bytes()
+    packer, unpacker = get_packer_unpacker(session_box, redis_connection, pubkey_bytes)
+    msg = pubkey_bytes+(await packer({'hello': 'friend'}))
+    async with aiohttp.client.ClientSession() as client:
+        async with client.post(url=f'{_constants.upstream_protocol}://{_constants.upstream_host}:{_constants.upstream_port}/signup', data=msg) as resp:
+            resp_bytes = await resp.read()
+            print(resp_bytes)
+
 
 async def test_ws():
     async with aiohttp.client.ClientSession() as client:
@@ -11,8 +23,6 @@ async def test_ws():
             print(msg)
             if msg.type in [aiohttp.WSMsgType.ERROR, aiohttp.WSMsgType.CLOSE, aiohttp.WSMsgType.CLOSED, aiohttp.WSMsgType.CLOSING]:
                 break
-while True:
-    try:
-        handlebars.multi_spawner(test_ws).join()
-    except KeyboardInterrupt:
-        break
+
+handlebars.multi_spawner(test_signup).join()
+handlebars.multi_spawner(test_ws).join()
