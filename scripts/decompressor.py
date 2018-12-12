d6a9d63554113e8bd2e71b5568f169d079833b90
Ian Daniher <itdaniher@gmail.com>
2018-12-12 02:37:04 +0000

    bump with decompressor script

---
+++ scripts/decompressor.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('./')
+import node_core
+import cbor
+
+import time
+
+for fname in sys.argv[1::]:
+    f = open(fname, 'rb').read()
+    start = time.time()
+    info = cbor.loads(f)
+    start = time.time()
+    decoded = node_core.decompress(**info)
+    end = time.time()
+    open('/dev/stdout', 'wb').write(decoded.tobytes())
