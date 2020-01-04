09af7715efc12eaac8197748157be29d5734e518
itdaniher <itdaniher@gmail.com>
2020-01-03 19:02:06 -0500

    rework tester to work on raw c64 blobs and cbor'd compressed np arrays of c64s

--- scripts/tester.py
+++ scripts/tester.py
@@ -2,15 +2,29 @@ import sys
 import time
 
 import cbor
-import node_core
 
 sys.path.append("./")
 
+
+from node_core import *
 
 for fname in [fname for fname in sys.argv[1::] if "--" not in fname]:
     print("analysing", fname)
     f = open(fname, "rb").read()
     start = time.time()
-    decoded = node_core.pulses_to_sample(node_core.block_to_pulses(cbor.loads(f)))
-    end = time.time()
-    print(end - start, decoded)
+    if f[0] == 0: # raw c64
+        uid = fname.split('/')[-1].lstrip('_').rstrip('.beep')
+        llen = len(f) // 8
+        iq = numpy.ndarray(llen, dtype='float32',buffer=f).view(dtype='complex64')
+        reading = SerializedReading(uid, cbor.dumps(compress(iq)))
+    elif f[0] == 0xa3: # cbor encoded compressed numpy array
+        uid = ulid2.generate_ulid_as_base32()
+        reading = SerializedReading(uid, f)
+    for f_ in [brickwall, lowpass]:
+        decoded = pulses_to_sample(block_to_pulses(reading, f_))
+        end = time.time()
+        d = None
+        for d in decoded:
+            print(end - start, f_, d)
+        if d:
+            break
